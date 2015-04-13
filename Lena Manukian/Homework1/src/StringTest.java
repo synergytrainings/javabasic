b15e45b6c21db40498cc46412a776e3743ed7e0a
Lena Manukyan <lena.manukyan@arm.synisys.com>
2015-04-13 15:29:39 +0400

    Homework1

---
+++ Lena Manukian/Homework1/src/StringTest.java
@@ -0,0 +1,46 @@
+
+public class StringTest {
+	private static boolean ifEquale(String s1, String s2){
+		//if s1 pointer == s2 pointer
+		//I don't check null pointers because of the compiler don't allow null pointers 
+		if(s1 == s2 )
+		{
+			return true ;
+		}
+		
+			if(s1.length() != s2.length())
+			{
+				return false;
+		
+			}
+		
+		
+		int equalSymbolCount = 0;
+		for(int i = 0; i< s1.length(); i++){
+	        //if corresponding symbols are equal then continue,else break loop
+			if(s1.indexOf(i) == s2.indexOf(i))
+			{
+				equalSymbolCount++;
+			}
+			else break;
+			
+		}// end of loop
+		
+		if(equalSymbolCount == s1.length())
+		return true ;
+		
+		else{ 
+			return false;
+		}
+	}//end of ifEquale()
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("");
+		String s2 = new String("");
+		
+		System.out.println(ifEquale(s1,s2));
+
+	}
+
+}
