87e0c3de66c3b4d1eb2e7bf6474aaf2416c7d8fb
Lena Manukyan <lena.manukyan@arm.synisys.com>
2015-05-15 12:27:37 +0400

    homework3

--- Lena Manukian/Homework3/Calculator.java
+++ Lena Manukian/Homework3/Calculator.java
@@ -5,9 +5,9 @@ public class Calculator {
 	public static void main(String[] args) {
 
 		System.out.println("please enter a number, an action and the a number to calculate result");
-		Scanner in = new Scanner(System.in);
-		try {
-
+		//Scanner in = new Scanner(System.in);
+		try(Scanner in = new Scanner(System.in)){
+			// Scanner in = new Scanner(System.in);
 			double d1 = in.nextDouble();
 
 			String action = in.next();
@@ -26,20 +26,34 @@ public class Calculator {
 				System.out.println(d1 * d2);
 				break;
 			case "/":
-				System.out.println(d1 / d2);
+				
+					System.out.println(d1 / d2);
+				
 				break;
 			default:
 				System.out.println("you entered the action that program does not handle");
 			}
+			
 
-		} catch (IllegalStateException e) {
+		} 
+		
+		catch (IllegalStateException e) {
 			System.out.println("there is an error with IO: Scanner is closed");
-		} catch (InputMismatchException e) {
-			System.out.println("cannot interpret your value as a number, please enter a valid value");
-		} finally {
-			// close the Scanner
-			in.close();
 		}
+		catch (InputMismatchException e) {
+			System.out.println("cannot interpret your value as a number, please enter a valid value");
+		} 
+		catch (java.lang.ArithmeticException e) {
+			System.out.println("error: you tried devide by 0");
+		} 
+		/*
+		finally {
+			if (in != null) {
+				in.close();
+			} else
+				System.out.println("there is an error with IO: Scanner is closed");
+		}
+		*/
 	}
 
 }
