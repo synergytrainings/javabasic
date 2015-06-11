93f52d3b10d1607892b29e7a6205ba4a9ca06082
Lena Manukyan <lena.manukyan@arm.synisys.com>
2015-06-11 12:19:14 +0400

    Homework5
    Homework5

--- Lena Manukian/Homework5/DataHierarchyWithHashMap.java
+++ Lena Manukian/Homework5/DataHierarchyWithHashMap.java
@@ -4,59 +4,108 @@ import java.util.List;
 import java.util.Map;
 
 public class DataHierarchyWithHashMap {
-
-	public static void main(String[] args) {
-		List<Integer> indicator;
-
-		// System.out.println(indicator);
-		// crate districts
-		Map<Integer, List<Integer>> district;
-
+	
+	private List<Integer> indicator;
+	private Map<Integer, List<Integer>> district;
+	private Map<Integer, Map<Integer, List<Integer>>> provinces;
+	
+	/**
+	 * setData() method fills provinces, districts and indicator's expected data with random values
+	 */
+	public  void  setData(){
 		// create provinces
-		Map<Integer, Map<Integer, List<Integer>>> provinces = new HashMap<Integer, Map<Integer, List<Integer>>>();
-
+		provinces = new HashMap<Integer, Map<Integer, List<Integer>>>();
+		
 		// fill provinces associated with district + indicator matrix
-		int addToIndex = 0;
-		for (int k = 0; k < 3; k++) {
-			district = new HashMap<Integer, List<Integer>>();
-			for (int j = 0; j < 3; j++) {
-				int size = (int) Math.random() + 4;
-				// fill indicators
-				indicator = new ArrayList<>();
-				for (int i = 0; i < size; i++) {
-					indicator.add((int) (Math.random() * 1000) + 4);
+				int addToIndex = 0;
+				for (int k = 0; k < 3; k++) {
+					district = new HashMap<Integer, List<Integer>>();
+					for (int j = 0; j < 3; j++) {
+						int size = (int) Math.random() + 4;
+						// fill indicators
+						indicator = new ArrayList<>();
+						for (int i = 0; i < size; i++) {
+							indicator.add((int) (Math.random() * 1000) + 4);
+						}
+						// fill districts
+						district.put(addToIndex, indicator);
+						addToIndex += indicator.get(indicator.size() - 1);
+
+					}
+					// fill provinces
+					provinces.put(k, district);
+
 				}
-				// fill districts
-				district.put(addToIndex, indicator);
-				addToIndex += indicator.get(indicator.size() - 1);
+		
+	}
+	/**
+	 * printSpecificDistricIndicatorValues(Integer id) method prints
+	 *  province->district->indicator's values for specific district
+	 * @param districID
+	 */
 
+	public void printSpecificDistricIndicatorValues(Integer districID){
+		
+		for(Map.Entry<Integer, Map<Integer,List<Integer>>> e: provinces.entrySet()){
+			//System.out.println("provinces " + e.getKey());
+			for(Map.Entry<Integer, List<Integer>> k: e.getValue().entrySet()){
+				//System.out.print("         ");
+				if( k.getKey() == districID ){
+					System.out.println("provinces " + e.getKey());
+					System.out.print("         ");
+				    System.out.println("district " + k.getKey());
+				    System.out.print("                 ");
+				    System.out.println(" indicator's values: " + k.getValue());
+			   }
 			}
-			// fill provinces
-			provinces.put(k, district);
-
 		}
-		// output the hierarchy
-		/*
-		for (Integer i : provinces.keySet()) {
-
-			System.out.println("provinces " + i);
-			for (Integer j : provinces.get(i).keySet()) {
-				System.out.print("         ");
-				System.out.println("district " + j);
-				System.out.print("                 ");
-				System.out.println(" indicator: " + provinces.get(i).get(j));
-			}
-		}
-*/
+	}
+	
+	/**
+	 * printDataHierarchy() method prints province->district->indicator's values all data
+	 */
+	public void printDataHierarchy(){
 		for(Map.Entry<Integer, Map<Integer,List<Integer>>> e: provinces.entrySet()){
 			System.out.println("provinces " + e.getKey());
 			for(Map.Entry<Integer, List<Integer>> k: e.getValue().entrySet()){
 				System.out.print("         ");
 				System.out.println("district " + k.getKey());
 				System.out.print("                 ");
-				System.out.println(" indicator: " + k.getValue());
+				System.out.println(" indicator's values: " + k.getValue());
 			}
 		}
+	}
+	
+	public void printTotalValueOfIndicators(){
+		for(Map.Entry<Integer, Map<Integer,List<Integer>>> e: provinces.entrySet()){
+			System.out.println("provinces " + e.getKey());
+			for(Map.Entry<Integer, List<Integer>> k: e.getValue().entrySet()){
+				System.out.print("         ");
+				System.out.println("district " + k.getKey());
+				System.out.print("                 ");
+				System.out.print(" indicator's total: ");
+				//count the total of indicator's values
+				List<Integer> list = k.getValue();
+				int sum = 0;
+				for(Integer i: list){
+					sum += i;
+				}
+				
+				System.out.println(sum);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		DataHierarchyWithHashMap data = new DataHierarchyWithHashMap();
+		
+		data.setData();
+		//data.printDataHierarchy();
+		
+		//data.printTotalValueOfIndicators();
+		data.printSpecificDistricIndicatorValues(0);
+		
 	}
 
 }
